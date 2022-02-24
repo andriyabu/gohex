09c08b98c030a91f8f6102ca3f762f58eac6f120
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2022-02-24 16:23:13 +0700

    Added Substraction, Multiplication, and Division function

--- internal/adapters/core/arithmetic/arithmetic.go
+++ internal/adapters/core/arithmetic/arithmetic.go
@@ -6,3 +6,15 @@ type Adapter struct {
 func (arith Adapter) Addition(a int32, b int32) (int32, error) {
 	return a + b, nil
 }
+
+func (arith Adapter) Substaction(a int32, b int32) (int32, error) {
+	return a - b, nil
+}
+
+func (arith Adapter) Multiplication(a int32, b int32) (int32, error) {
+	return a * b, nil
+}
+
+func (arith Adapter) Division(a int32, b int32) (int32, error) {
+	return a / b, nil
+}
