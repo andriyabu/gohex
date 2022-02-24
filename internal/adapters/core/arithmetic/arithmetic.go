ae942751e1fcb788918fbaab2103ad4e454dca10
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2022-02-24 16:25:04 +0700

    changed name of Substaction to Substraction function

--- internal/adapters/core/arithmetic/arithmetic.go
+++ internal/adapters/core/arithmetic/arithmetic.go
@@ -7,7 +7,7 @@ func (arith Adapter) Addition(a int32, b int32) (int32, error) {
 	return a + b, nil
 }
 
-func (arith Adapter) Substaction(a int32, b int32) (int32, error) {
+func (arith Adapter) Substraction(a int32, b int32) (int32, error) {
 	return a - b, nil
 }
 
--- internal/ports/core.go
+++ internal/ports/core.go
@@ -2,7 +2,7 @@ package ports
 
 type AritmeticPort interface {
 	Addition(a int32, b int32) (int32, error)
-	Substaction(a int32, b int32) (int32, error)
+	Substraction(a int32, b int32) (int32, error)
 	Multiplication(a int32, b int32) (int32, error)
 	Division(a int32, b int32) (int32, error)
 }
