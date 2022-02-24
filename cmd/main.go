d7793d06a291dbda55a81442b8a6453e67710a6c
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2022-02-24 17:44:57 +0700

    Created app.go and add APIPort interface

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"hex/internal/adapters/core/arithmetic"
+)
 
+func main() {
+	arithAdapter := arithmetic.NewAdapter()
+
+	result, err := arithAdapter.Addition(1, 3)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
 }
